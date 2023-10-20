d31d7b90508ec2b69bb5d0999e77a9a5d84acf47
Nelsocol <collinnelson509@gmail.com>
2023-10-20 10:14:58 -0700

    Created new Blazor project and integrated/refactored old database builder code. Added validation step to ingestion procedure

---
+++ AirportTracker/DatabaseBuilder/CypherBindings.cs
@@ -0,0 +1,85 @@
+﻿using Neo4j.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseBuilder
+{
+    internal class CypherBindings : IDisposable
+    {
+
+        private readonly IDriver _driver;
+
+        public CypherBindings(string uri, string user, string password)
+        {
+            _driver = GraphDatabase.Driver(uri, AuthTokens.Basic(user, password));
+        }
+
+        public void Dispose()
+        {
+            _driver?.Dispose();
+        }
+
+        private async void RunCypher(string cypher)
+        {
+            using var session = _driver.AsyncSession();
+            await session.ExecuteWriteAsync(
+                async tx => {
+                    await tx.RunAsync(cypher);
+                }
+            );
+        }
+
+        public void FlushDB()
+        {
+            RunCypher(
+                @"MATCH (n)
+			DETACH DELETE n"
+            );
+        }
+
+        public void CreateAirport(List<string> line)
+        {
+            RunCypher(
+                $@"CREATE (a:Airport {{id: {line[0]}, name: ""{line[1]}"", city: ""{line[2]}"", country: ""{line[3]}"", iata: ""{line[4]}"", iaco: ""{line[5]}"", latitude: {line[6]}, longitude: {line[7]}, altitude: {line[8]}, timezone: ""{line[9]}"", dst: ""{line[10]}"", tztime: ""{line[11]}"",type: ""{line[12]}"", source: ""{line[13]}""}})"
+            );
+        }
+
+        public string CreateRoute(List<string> line)
+        {
+            //NOTE: Mostly fill in the blank here, but airline ID's with the value \N are replaced with -1 so that the field can be stored as an int.
+            RunCypher(
+                $@"MATCH (source:Airport {{id: {line[3]}}}), (dest:Airport {{id: {line[5]}}})
+			CREATE (source)-[:ROUTE {{airline: ""{line[0]}"", airlineid: {(line[1] == "\\N" ? -1 : line[1])}, source: ""{line[2]}"", sourceid: {line[3]}, dest: ""{line[4]}"", destid: {line[5]}, codeshare: ""{line[6]}"", stops: {line[7]}, equipment: ""{line[8]}""}}]->(dest)"
+            );
+            return "";
+        }
+
+        public async void Validation(int expectedNodes, int expectedRelations) 
+        {
+            using var session = _driver.AsyncSession();
+            var nodeCount =  (await session.ExecuteWriteAsync(
+                async tx => {
+                    var result = await tx.RunAsync($@"MATCH (e:Airport)
+                    RETURN count(e)");
+
+                    return (await result.SingleAsync())[0].As<int>();
+                }
+            )).As<int>();
+
+            var relationsCount = (await session.ExecuteWriteAsync(
+                async tx => {
+                    var result = await tx.RunAsync($@"MATCH (:Airport)-[r:ROUTE]->(:Airport)
+                    RETURN count(r)");
+
+                    return (await result.SingleAsync())[0].As<int>();
+                }
+            )).As<int>();
+
+            Console.WriteLine($"Node Validation: {nodeCount} of {expectedNodes} nodes loaded.");
+            Console.WriteLine($"Relations Validation: {relationsCount} of {expectedRelations} routes loaded.");
+        }
+    }
+}
---
+++ AirportTracker/DatabaseBuilder/Program.cs
@@ -0,0 +1,107 @@
+﻿using DatabaseBuilder;
+using Neo4j.Driver;
+
+public class AirportDataIngestor
+{
+    public static void Main()
+    {
+        //If true, will only load the first 300 airports and routes that connect them. Makes things way faster for testing.
+        bool quickModeEnabled = false;
+        if (quickModeEnabled)
+        {
+            Console.WriteLine("Running with quick mode enabled. Will only load the first 300 airports and routes that connect them.");
+        }
+
+        using var cypher = new CypherBindings("bolt://localhost:7687", "neo4j", "password");
+
+        FlushDB(cypher);
+
+        Console.WriteLine("Beginning DB rebuild...");
+
+        int progressCounter;
+        HashSet<string> safeIDs;
+
+        AddAirportNodes(quickModeEnabled, cypher, out progressCounter, out safeIDs);
+
+        progressCounter = AddRouteRelations(quickModeEnabled, cypher, safeIDs);
+
+        Console.WriteLine("Database rebuild complete.");
+
+        cypher.Validation(quickModeEnabled ? 300 : 7698, quickModeEnabled ? 1219 : 66771);
+
+        Thread.Sleep(1000);
+    }
+
+    private static int AddRouteRelations(bool quickModeEnabled, CypherBindings cypher, HashSet<string> safeIDs)
+    {
+        int progressCounter;
+        Console.WriteLine("Adding route relations...");
+        Console.Write("                     ]\r[");
+        progressCounter = 0;
+        foreach (string line in File.ReadLines("./routes.csv"))
+        {
+            List<string> tokens = line.Replace("\"", "").Split(',').ToList(); //" Characters also removed from this input to make string formatting work.
+
+            progressCounter++;
+            if (progressCounter % (67663 / 20) == 0)
+            {
+                Console.Write("█");
+            }
+
+            if (tokens[3] == "\\N" || tokens[5] == "\\N") //Skip any records that don't have a valid airport ID for either their source or destination
+                continue;
+
+            if (quickModeEnabled && (!safeIDs.Contains(tokens[3]) || !safeIDs.Contains(tokens[5])))
+                continue; //In safe mode, skips the slow step if the route isn't between two airports in the first 300
+
+            cypher.CreateRoute(tokens);
+            Thread.Sleep(10);
+        }
+        Console.WriteLine("]\nRoute relations added.");
+        return progressCounter;
+    }
+
+    private static void AddAirportNodes(bool quickModeEnabled, CypherBindings cypher, out int progressCounter, out HashSet<string> safeIDs)
+    {
+        Console.WriteLine("Adding airport nodes...");
+        Console.Write("                     ]\r[");
+        progressCounter = 0;
+        safeIDs = new HashSet<string>();
+        foreach (string line in File.ReadLines("./airports.csv"))
+        {
+            List<string> tokens = line.Replace("\"", "").Split(',').ToList(); //All " characters are removed to avoid issues with string formatting later.
+                                                                              //Accounts for the edge case in which airport names contain a comma, which splits the second entry into two.
+            if (tokens.Count > 14)
+            {
+                tokens.Insert(1, $"{tokens[1]} {tokens[2]}");
+                tokens.RemoveRange(2, 2);
+            }
+            cypher.CreateAirport(tokens);
+            Thread.Sleep(10);
+
+            progressCounter++;
+            if (progressCounter % ((quickModeEnabled ? 300 : 7698) / 20) == 0)
+            {
+                Console.Write("█");
+            }
+
+            if (quickModeEnabled)
+            {
+                safeIDs.Add(tokens[0]);
+            }
+
+            if (quickModeEnabled && progressCounter >= 300)
+                break; //Early exit for quick mode
+        }
+        Console.WriteLine("]\nAirport nodes added.");
+    }
+
+    private static void FlushDB(CypherBindings cypher)
+    {
+        Console.WriteLine("Flushing DB...");
+        cypher.FlushDB();
+        Console.WriteLine("DB flush completed.\n");
+
+        Thread.Sleep(100);
+    }
+}
