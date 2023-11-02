d63a9b1c4b2779d1c31ebe4aab9d0947981b8abe
Trent Bultsma <trentbultsma@gmail.com>
2023-11-02 16:04:15 -0700

    create local algorithm development testing project

---
+++ AirportTracker/LocalTestingProject/DatabaseManager.cs
@@ -0,0 +1,34 @@
+﻿using Neo4j.Driver;
+using System;
+
+namespace AirportTracker
+{
+	public class DatabaseManager
+	{
+		private readonly IDriver _driver;
+
+		public DatabaseManager()
+		{
+			_driver = GraphDatabase.Driver("bolt://localhost:7687", AuthTokens.Basic("neo4j", "password"));
+		}
+
+		public string GetAirport(string city)
+		{
+			return RunCypher(@"MATCH (n) 
+				RETURN n.name LIMIT 1").Result;
+		}
+
+		private async Task<string> RunCypher(string cypher)
+		{
+			using var session = _driver.AsyncSession();
+			string result = await session.ExecuteWriteAsync(
+				async tx => {
+                    var localResult = await tx.RunAsync(cypher);
+					var record = await localResult.SingleAsync();
+                    return record[0].As<string>();
+                }
+			);
+			return result;
+		}
+	}
+}
---
+++ AirportTracker/LocalTestingProject/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace AirportTracker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            DatabaseManager dbManager = new DatabaseManager();
+            Console.WriteLine(dbManager.GetAirport("test"));
+        }
+    }
+}
