ef31cf475cacfb12f0da21310af66c65943cf4dc
Trent Bultsma <trentbultsma@gmail.com>
2023-11-03 14:10:06 -0700

    change airport getter from a city name to return a list of airports in that city

--- AirportTracker/LocalTestingProject/DatabaseManager.cs
+++ AirportTracker/LocalTestingProject/DatabaseManager.cs
@@ -1,4 +1,5 @@
 ﻿using Neo4j.Driver;
+using Neo4j.Driver.Preview.Mapping;
 using System;
 using System.Collections.Generic;
 using System.Security.Cryptography;
@@ -14,46 +15,7 @@ namespace AirportTracker
 		{
 			_driver = GraphDatabase.Driver("bolt://localhost:7687", AuthTokens.Basic("neo4j", "password"));
 		}
-
-		public Airport? GetAirport(string city)
-		{
-			IReadOnlyDictionary<string, object> airportQueryResult;
-            try
-			{
-				airportQueryResult = RunCypher(
-					@$"MATCH (n:Airport {{city: '{city}'}})
-					RETURN 
-						n.name, n.id, n.city, n.country, 
-						n.iata, n.iaco, n.latitude, 
-						n.longitude, n.altitude, 
-						n.timezone, n.dst, n.tztime, 
-						n.type, n.source 
-					LIMIT 1").Result;
-			}
-			catch (System.AggregateException)
-			{
-				return null;
-			}
-
-            Airport airport = new Airport();
-            airport.Name = airportQueryResult["n.name"].As<string>();
-			airport.Id = airportQueryResult["n.id"].As<int>();
-			airport.City = airportQueryResult["n.city"].As<string>();
-			airport.Country = airportQueryResult["n.country"].As<string>();
-			airport.IATA = airportQueryResult["n.iata"].As<string>();
-			airport.IACO = airportQueryResult["n.iaco"].As<string>();
-			airport.Latitude = airportQueryResult["n.latitude"].As<float>();
-			airport.Longitude = airportQueryResult["n.longitude"].As<float>();
-			airport.Altitude = airportQueryResult["n.altitude"].As<float>();
-			airport.Timezone = airportQueryResult["n.timezone"].As<string>();
-			airport.DST = airportQueryResult["n.dst"].As<string>();
-			airport.TzTime = airportQueryResult["n.tztime"].As<string>();
-			airport.Type = airportQueryResult["n.type"].As<string>();
-			airport.Source = airportQueryResult["n.source"].As<string>();
-
-            return airport;
-		}
-
+		
 		// TODO can do a shortest path, but for now just take the first one
 		public List<Route> GetPathBetweenAirports(Airport source, Airport destination)
 		{
@@ -70,14 +32,37 @@ namespace AirportTracker
             return path;
 		}
 
-		// TODO: note, there are problems if the query does not limit the number of returns to 1
-		private async Task<IReadOnlyDictionary<string,object>> RunCypher(string cypher)
+		public List<Airport> GetAirports(string city)
+		{
+			return GetAirportsAsync(city).Result;
+		}
+
+        private async Task<List<Airport>> GetAirportsAsync(string city)
+        {
+            using var session = _driver.AsyncSession();
+            return await session.ExecuteWriteAsync(
+                async tx => {
+                    var queryResult = await tx.RunAsync(
+						 @$"MATCH (n:Airport {{city: '{city}'}}) RETURN n");
+                    var airports = queryResult.AsObjectsAsync<Airport>();
+                    List<Airport> resultAirports = new List<Airport>();
+                    await foreach (Airport airport in airports)
+					{
+						resultAirports.Add(airport);
+					}
+					return resultAirports;
+                }
+            );
+        }
+
+        private async Task<IReadOnlyDictionary<string,object>> RunCypher(string cypher)
 		{
 			using var session = _driver.AsyncSession();
 			var result = await session.ExecuteWriteAsync(
 				async tx => {
                     var localResult = await tx.RunAsync(cypher);
-					var record = await localResult.SingleAsync();
+                    // note: SingleAsync() in this line means queries must have exactly 1 return value
+                    var record = await localResult.SingleAsync();
                     return record.Values;
                 }
 			);
--- AirportTracker/LocalTestingProject/Program.cs
+++ AirportTracker/LocalTestingProject/Program.cs
@@ -5,16 +5,25 @@
         public static void Main(string[] args)
         {
             DatabaseManager dbManager = new DatabaseManager();
-            Airport? airport1 = dbManager.GetAirport("Seattle");
-            Airport? airport2 = dbManager.GetAirport("Spokane");
-            if (airport1 != null && airport2 != null)
-            {
-                Console.WriteLine(airport1.Name);
-                Console.WriteLine(airport2.Name);
 
-                dbManager.GetPathBetweenAirports(airport1, airport2);
+            List<Airport> seattleAirports = dbManager.GetAirports("Seattle");
+            foreach (Airport airport in seattleAirports)
+            { 
+                Console.WriteLine(airport.Name);
             }
 
+            List<Airport> nonexistentAirports = dbManager.GetAirports("not a real city name");
+            Console.WriteLine(nonexistentAirports.Count);
+
+            //Airport? airport1 = dbManager.GetAirport("Seattle");
+            //Airport? airport2 = dbManager.GetAirport("Spokane");
+            //if (airport1 != null && airport2 != null)
+            //{
+            //    Console.WriteLine(airport1.Name);
+            //    Console.WriteLine(airport2.Name);
+            //
+            //    dbManager.GetPathBetweenAirports(airport1, airport2);
+            //}
         }
     }
 }
